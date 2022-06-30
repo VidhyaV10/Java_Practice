e74b021d652cd076f2c2fd442a5f3570f2a81604
VidhyaV10 <105286001+VidhyaV10@users.noreply.github.com>
2022-06-30 12:13:00 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,69 @@
+/*  JFM1T2_Assignment1:
+
+     Write a program to perform the below actions and print the result.
+     1. Difference of 2 numbers (30 and 10)
+     2. Product of 2 numbers (45 and 2)
+     3. Division of 2 numbers (600 and 10)
+     4. Increment and Decrement the number (15)
+     5. Remainder of 2 numbers (14 and 5)
+     Prompt the user input from the terminal.
+*/
+
+//import statements for java program to read inputs using Scanner class
+//import statements for java program to read inputs using Scanner class
+import java.util.Scanner;
+
+public class Operations {
+
+//main method
+  public static void main(String args[])
+  {
+
+/*
+  Use the scanner class to provide input at execution time using scanner object
+  Scanner sc=new Scanner(System.in);
+*/
+  Scanner sc=new Scanner(System.in);
+/*
+  Take input from user
+  System.out.println("Enter first number: ");
+  int a=sc.nextInt();
+*/
+  System.out.println("Enter first number: ");
+  int a=sc.nextInt();
+  System.out.println("Enter second number: ");
+  int b=sc.nextInt();
+//calculate difference,product,division,increment and remainder 
+  int diff=a-b;
+  System.out.println("Difference of two numbers: "+diff);
+    
+  System.out.println("Enter first number: ");
+  int c=sc.nextInt();
+  System.out.println("Enter second number: ");
+  int d=sc.nextInt();
+  int product= c*d;
+  System.out.println("Product of 2 numbers: "+product);
+
+  System.out.println("Enter first number: ");
+  int e=sc.nextInt();
+  System.out.println("Enter second number: ");
+  int f=sc.nextInt();
+  int division= e/f;
+  System.out.println("Division of 2 numbers: "+division);
+
+  System.out.println("Enter a number: ");
+  int x=sc.nextInt();
+  int incre=x++;
+  int decre=x--;
+  System.out.println("Increment of the number is: "+incre+ " and decrement of the number is: "+decre);
+
+  System.out.println("Enter first number: ");
+  int y=sc.nextInt();
+  System.out.println("Enter second number: ");
+  int z=sc.nextInt();
+  int rem= y%z;
+  System.out.println("remainder30 of 2 numbers: "+rem);
+//print appropriate result for all operations
+
+    }
+}
