d195b51fef61c604a0b6ce39d513817000f6246c
VidhyaV10 <105286001+VidhyaV10@users.noreply.github.com>
2022-07-05 12:58:58 +0530

    Add files via upload

---
+++ Exercise1 (3).java
@@ -0,0 +1,24 @@
+//  Write a program to print the elements of an array present on even position
+
+import java.util.Scanner;
+class Exercise1
+{
+   public static void main(String args[]) 
+  {
+    int n,i;
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter array size: ");
+    n=sc.nextInt();
+    int arr[]=new int[n];
+    System.out.println("Enter array elements: ");
+    for(i=0;i<n;i++)
+      {
+        arr[i]=sc.nextInt();
+      }
+    for(i=0;i<n;i=i+2)
+    {
+    System.out.println("the elements at even position are: " +arr[i]);
+		    
+   }
+ }
+}  
---
+++ Sample1 (3).java
@@ -0,0 +1,24 @@
+// Complete and execute java Program for printing only odd elements of a single dimensional array
+class Sample1 {
+
+	public static void main(String args[]) {
+
+		//Initialize array  
+        	int [] arr = new int [] {22, 12, 7, 75, 56}; 
+    
+
+        	//Loop through the array using for-each loop
+        	for (int i:arr) 
+          {  
+
+           	if((i%2)!=0) 
+            {
+               int j=i;
+              //Write print statement here
+              System.out.println(j);
+            }
+            
+               
+        }  
+   }
+}
