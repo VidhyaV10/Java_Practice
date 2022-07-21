2bc5c1cc4d9747f79ee14bbd5a7fb8798569b0e4
VidhyaV10 <105286001+VidhyaV10@users.noreply.github.com>
2022-07-21 17:43:23 +0530

    Add files via upload

---
+++ ArithmeticException.java
@@ -0,0 +1,18 @@
+/* 1. Write a program which will illustrate ArthmeticException. Create a parameterized method which can take two values and print division of two numbers. 
+Please write your comments regarding ArithmeticException such as "When ArithmeticException will occur in this program?"
+*/
+public class ArithmeticException 
+{
+   void division(int a,int b)
+  {
+    int c=a/b;
+    System.out.println("division of Two numbers: "+c);
+  }
+  public static void main(String[] args)
+  {
+    ArithmeticException ex=new ArithmeticException();
+    ex.division(10,0);//Here, we are dividing 10 by 0,hence ArithmeticException occurs in this program
+  }
+}
+
+
---
+++ ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,27 @@
+/* 3. Write a program which will illustrate ArrayIndexOutOfBoundsException. 
+   Create an array variable with n size, accept values from user and store values into array, finally print all values on console. 
+   Please write your comments regarding ArrayIndexOutOfBoundsException such as " When ArrayIndexOutOfBoundsException occur in this program?"
+*/
+import java.util.Scanner;
+public class ArrayIndexOutOfBoundsException 
+{
+   public static void main(String args[]) 
+    {
+      Scanner sc=new Scanner(System.in);
+      int[] arr = new int[5];//array length is 5
+      System.out.println("Enter array elements: ");
+
+      for(int i=0;i<=5;i++)//but,we are trying to access 6 elements.Hence,ArrayIndexOutOfBoundsException occurs here 
+        {
+          arr[i]=sc.nextInt();
+        }
+      System.out.println("array elements are");
+      for(int i=0;i<=5;i++)
+        {
+          System.out.println(arr[i]);
+        }
+      
+      
+    }
+}
+
